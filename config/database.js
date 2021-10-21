77a41cb97f373726e3af8c0f74c93023b56578a4
Alex-Hemming <72031268+Alex-Hemming@users.noreply.github.com>
2021-10-21 23:58:07 +0100

    third

--- config/database.js
+++ config/database.js
@@ -1,30 +1,28 @@
-
-const parse= require('pg-connection-string').parse;
+const parse = require("pg-connection-string").parse;
 
 module.exports = ({ env }) => {
-if (env('NODE_ENV')=== 'production') {
-
-  const config =parse(process.env.DATABSE_URL)     
+  if (env("NODE_ENV") === "production") {
+    const config = parse(process.env.DATABSE_URL);
     return {
-defaultConnection:'default',
-connections: {
-  default: {
-    connector: "bookshelf",
-    settings: {
-client: "postgres",
-host:config.host,
-post: config.port,
-database: config.database,
-username: config.user,
-password: config.password,
-
-  },
-  options: {
-    ssl:false,
+      defaultConnection: "default",
+      connections: {
+        default: {
+          connector: "bookshelf",
+          settings: {
+            client: "postgres",
+            host: config.host,
+            post: config.port,
+            database: config.database,
+            username: config.user,
+            password: config.password,
+          },
+          options: {
+            ssl: false,
+          },
+        },
+      },
+    };
   }
-}
-  }
-}}
   return {
     defaultConnection: "default",
     connections: {
