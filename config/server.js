3a0251f4216e6d3398e73fa545dea7421a436f70
Alex-Hemming <72031268+Alex-Hemming@users.noreply.github.com>
2021-10-22 11:18:56 +0100

    Update database config

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: "https://ellaphotos.herokuapp.com/",
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'e93f03069e0ca427a9d938277927078e'),
