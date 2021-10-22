3ed4c5bea147cc62c6854593b0523fffd64f90c3
Alex-Hemming <72031268+Alex-Hemming@users.noreply.github.com>
2021-10-22 11:07:19 +0100

    test

--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,15 @@
-module.exports = ({ env }) => ({
-  upload: {
-    provider: "cloudinary",
-    providerOptions: {
-      cloud_name: "dt6mvxx9u",
-      api_key: "249732929916245",
-      api_secret: "FViuQkJvcY23fjB-y1GW8cXees8",
-    },
-  },
-});
+module.exports = ({ env }) => {
+  if (env("NODE_ENV") === "production") {
+    return {
+      upload: {
+        provider: "cloudinary",
+        providerOptions: {
+          cloud_name: "dt6mvxx9u",
+          api_key: "249732929916245",
+          api_secret: "FViuQkJvcY23fjB-y1GW8cXees8",
+        },
+      },
+    };
+  }
+  return {};
+};
